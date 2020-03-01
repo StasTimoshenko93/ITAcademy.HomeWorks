f9b21fc46a509018db12be29c6dfa186ca533592
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-03-01 13:37:24 +0300

    added algorithm

---
+++ Solution/HW.02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Чтение файла по пути:
+            //2. Запись файла в переменную типа string textReaderResult;
+            //3. Создания массива типа string arrayOfTextResult путем дробления текста по пробелам;
+            //4. Создания массива типа byte imageBytes  длиной равной массива типа string
+            //5. Перебор массива типа string в цикле;
+            //    5.1 Конвертирование переменной по индексу типа string в byte и запись в переменную  типа byte binary;
+            //    5.2 Запись переменных типа byte binary в массив типа byte imageBytes;
+            //6. Запись массива типа byte imageBytes в файл image.png;
+            //7. Использование метода dispose();
+        }
+    }
+}
