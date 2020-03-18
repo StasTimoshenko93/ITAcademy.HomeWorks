22c44d6cc1cef05a0dbca518c064b18dee8afdf7
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-03-18 21:30:12 +0300

    added task1

---
+++ Solution/HW05.Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW05.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet="";
+            for (int i = 122; i >=65; i--)
+            {
+                char letter = Convert.ToChar(i);
+                alphabet += letter.ToString();
+            }
+            Console.WriteLine(alphabet.Remove(26,6).Insert(26,"   ")) ;
+        }
+    }
+}
