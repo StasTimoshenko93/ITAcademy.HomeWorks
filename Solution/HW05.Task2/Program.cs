98c8827211fdb7a722e27c8b5117da8114dffe84
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-03-18 22:11:20 +0300

    added task2

---
+++ Solution/HW05.Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HW05.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start game");
+            Console.WriteLine("push the button");
+            Console.WriteLine("push q to finish game");
+            bool flag = true;
+            while (flag)
+            {
+                string pushButton = Console.ReadLine().ToLower();
+                switch (pushButton)
+                {
+                    case "w":
+                        Console.WriteLine("go straight");
+                        break;
+                    case "s":
+                        Console.WriteLine("go back");
+                        break;
+                    case "a":
+                        Console.WriteLine("turn right");
+                        break;
+                    case "d":
+                        Console.WriteLine("turn left");
+                        break;
+                    case "q":
+                        Console.WriteLine("Game over");
+                        flag = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("do nothing");
+                        break;
+                }
+            }
+        }
+    }
+}    
